fb56d0a18d93e7d0ccfe9d57376d062bdd63898e
RAJESH !!! <muthurajesht041@gmail.com>
2026-08-03 00:11:55 +0530

    Fix base path for GitHub Pages + simplify deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '/',
+    base: '/My_Portfolio_Rajesh/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
